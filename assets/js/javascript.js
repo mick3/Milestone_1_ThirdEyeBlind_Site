ebfd46cc3334c2c782c7572b2740e9e8533c6b26
mick3 <mvwebber3@gmail.com>
2019-10-02 18:11:43 -0400

    added more changes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,8 @@
+
+/*Code credited to Anthony O'Brien, former Channel-lead
+for User-Centric Front End and current Code Institute Student.
+This code collapses the navbar after selecting one of the options to 
+navigate to.*/
 $(".navbar-nav li a").on("click", function(){
     $(".navbar-collapse").collapse("hide");
 });
